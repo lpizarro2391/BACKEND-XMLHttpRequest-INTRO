aa215f543a922ca8aa91eb6bc5b1f8ec5ce48c44
lpizarro2391 <lourdespizarro@gmail.com>
2020-01-22 21:25:24 -0600

    Ejemplos de peticiones sincrónicas y asincrónicas usando jQuery

--- app.js
+++ app.js
@@ -1,34 +1,36 @@
-(function($){
-  var inputUser = $('#username')
-  var URL = 'https://api.github.com/users/'
-  inputUser.on('keypress',function(e){
-    if(e.which ===13){
-      var username = $(this). val(). trim().toLowerCase()
-      if(username !==""){
-        var XHR = new XMLHttpRequest()
-        var URLUser = URL + username
-        XHR.open('GET',URLUser)
-        XHR.onreadystatechange = function(){
-          if(XHR.readyState === 4){
-            var data = JSON.parse(XHR.responseText)
-            if(data.message && data.message =="Not Found"){
-              alert("El usuario no existe en Github")
-            }else {
-              $('img').attr('src',data.avatar_url)
-              $('.name').text(data.name)
-              $('.username').text(data.login)
-              $('.email').text(data.email)
-              $('.company').text(data.company)
-              $('.location').text(data.location)
-              $('.followers').text(data.followers)
-            }
-          }
-        }
-        XHR.send(null)
-      } else {
-      alert('Debe ingresar un nombre de usuario')
-    }
+var imputUser = $('#username')
+inputUser.on('keypress',function(e){
+  if(e.which ===13){
+    ajaxAsync()
   }
 })
 
-})(jQuery)
+function ajaxAsync(){
+  var URL = 'http://api.github.com/users/'
+  var username = inputUser.val().trim().tolowerCase()
+  if(username !==""){
+    var URLUser =  URL + username
+    $.ajax({
+      url: URLUser,
+      type:'GET',
+      async:true,
+      sucess: function(response){
+        var data = response
+        if(data.message && data.message =="Not Found"){
+          alert('el usuario no existe en github')
+        }else {
+          $('img').attr('src',data.avatar_url)
+             $('.name').text(data.name)
+             $('.username').text(data.login)
+             $('.email').text(data.email)
+             $('.company').text(data.company)
+             $('.location').text(data.location)
+             $('.followers').text(data.followers)
+        }
+      }
+    })
+
+  }else {
+    alert('Debe de ingresar un nombre de usuario')
+  }
+  }(jQuery)
